852509c6dbe6f21f2a267dcce5f8b8c9849f6ec9
kim8586 <tin8tin5@gmail.com>
2026-04-01 14:15:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -963,7 +963,7 @@ onClick={() => setShowUserMenu(!showUserMenu)}
 style={{
   background:"white",
   color:"#333",
-  padding:"8px 6px",
+  padding:"4px 4px",
   borderRadius:"6px",
   fontSize:"13px",
   fontWeight:"bold",
